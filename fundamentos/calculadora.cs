93790aac2d0ae77f908aa99b1191c09c2bfca1f9
David Duirs <duirsdevelopment@gmail.com>
2024-11-30 20:27:24 +0100

    calculadora.cs final

--- fundamentos/calculadora.cs
+++ fundamentos/calculadora.cs
@@ -1,32 +1,32 @@
-// Crea un programa que simule el funcionamiento de una calculadora.
 Console.WriteLine("¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨");
 Console.WriteLine("         Calculadora activada");
 Console.WriteLine("_______________________________________");
-int? x;
-int? y;
+int x;
+int y;
 do
 {
-    Console.WriteLine("Por favor, introduzca un número positivo:");
+    Console.Write("Por favor, introduce un número positivo: ");
     x = int.Parse(Console.ReadLine());
 }
 while (x < 1);
 do
 {
-    Console.WriteLine("Introduzca otro número positivo:");
+    Console.Write("Introduce otro número positivo: ");
     y = int.Parse(Console.ReadLine());
 }
 while (y < 1);
-int? c;
+int c;
 do
 {
     Console.WriteLine();
-    Console.WriteLine("Elege operación:    Pulsa:");
-    Console.WriteLine("     Suma           -  1");
-    Console.WriteLine("     Resta          -  2");
-    Console.WriteLine("     Multiplicación -  3");
-    Console.WriteLine("     División       -  4");
+    Console.WriteLine("     Elige operación:    Pulsa:");
+    Console.WriteLine("     ¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨    ¨¨¨¨¨¨");
+    Console.WriteLine("         Suma              1");
+    Console.WriteLine("         Resta             2");
+    Console.WriteLine("         Multiplicación    3");
+    Console.WriteLine("         División          4");
     Console.WriteLine();
-    Console.WriteLine("     Salir          -  5");
+    Console.WriteLine("         Salir             5");
     Console.WriteLine();
     c = int.Parse(Console.ReadLine());
     Console.WriteLine("¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨");
@@ -34,15 +34,18 @@ do
     {
         case < 1:
         case > 5:
+            Console.WriteLine("         ¡" + c + " no es válido!");
+            Console.WriteLine("");
+            Console.WriteLine("     Elige una opción válida del menú:");
             break;
         case 1:
             Console.WriteLine("     Resultado de sumar  " + x + "  más  " + y + "  =  " + (x + y));
             break;
         case 2:
-            Console.WriteLine("     Resultado de restar  " + x + "  menos " + y + " =  " + (x - y));
+            Console.WriteLine("     Resultado de restar  " + x + "  menos  " + y + "  =  " + (x - y));
             break;
         case 3:
-            Console.WriteLine("     Resultado de  " + x + "  muliplicado por " + y + "  =  " + (x * y));
+            Console.WriteLine("     Resultado de  " + x + "  multiplicado por  " + y + "  =  " + (x * y));
             break;
         case 4:
             Console.WriteLine("     Resultado de  " + x + "  dividido por  " + y + "  =  " + (x / y));
@@ -52,6 +55,5 @@ do
             break;
     }
     Console.WriteLine("_______________________________________");
-    if (c < 1 || c > 5) Console.WriteLine("Elege un opción valido del menú:");
 }
 while (c != 5);
