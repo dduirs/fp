edc32eef24e020321da9edd77e538d5dbfe96833
David Duirs <duirsdevelopment@gmail.com>
2025-01-07 10:03:48 +0100

    Rename Temperaturas.java to Temperatura.java

--- programación/Temperatura.java
+++ programación/Temperatura.java
@@ -1,7 +1,7 @@
 import java.util.Objects;
 import java.util.Scanner;
 
-public class Temperaturas {
+public class Temperatura {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
