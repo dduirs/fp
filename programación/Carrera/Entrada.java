9631a668788e68407cff87ac358ee6d037abe467
David Duirs <duirsdevelopment@gmail.com>
2025-01-19 02:29:55 +0100

    Actualizar Entrada.java

--- programación/Carrera/Entrada.java
+++ programación/Carrera/Entrada.java
@@ -1,37 +1,39 @@
-import Metodos.Coche;
-import Metodos.Carrera;
+import Modelos.Coche;
+import Modelos.Carrera;
 import java.util.Scanner;
 
 public class Entrada {
 
     static Scanner in = new Scanner(System.in);
 //    static String[] infoCoches = new String[]{"Escribe la marca del coche: ","modelo: ","matricula: ","cv: ","cc: "};
+
     public static void main(String[] args) {
         Carrera carrera = new Carrera();
         String a;
-        Coche coche1 = new Coche("Seat", "Arona", "YOU123",95,999,0,0);
+        Coche coche1 = new Coche("Seat", "Arona", "YOU123",95,999,182, 11.5, 0,0);
 //        https://www.autobild.es/coches/seat/arona/arona-5-2021/10-tsi-95-cv/ficha-tecnica
-        Coche coche2 = new Coche("Maserati", "Quattroporte", "WINNER1",580, 3799,
-                0,10);
+        Coche coche2 = new Coche("Maserati", "Quattroporte", "WINNER1",580, 3799, 326, 4.5,
+                0,0);
 //        https://www.autobild.es/coches/maserati/quattroporte/quattroporte-trofeo-4-2020/v8-47247/ficha-tecnica
-//        coche1 = (
-////            System.out.println(" = " + coche.acelerar(18));
-        coche1.print();
+        coche1.print("yellow");
         System.out.println();
-        coche2.print();
+        coche2.print("blue");
         System.out.printf("%n¿Quieres usar estes coche?   (S o N): ");
         a = in.next();
         if(a.equalsIgnoreCase("s")) {
-            carrera.iniciarCarrera(coche1,coche2);
+            System.out.println("Entra la longitud de la carrera: ");
+            carrera.setKmTotales(in.nextInt());
+            System.out.println("Entra el numero de vueltas: ");
+            carrera.setVueltas(in.nextInt());
+            Coche ganador = carrera.iniciarCarrera(coche1,coche2);
+            if(ganador.getKms()<carrera.getKmTotales()){
+                System.out.println("\n .........La carrera ha sido extendida........");
+                ganador = carrera.laCarrera(coche1, coche2);
+            }
+            System.out.printf(" El ganado ha sido el coche con matrícula %s, ¡Enhorabuena!", ganador.getMatricula());
         }
         else{
-            System.out.println("Ponte en contacto con los organizadores");
+            System.out.println("Pone en contacto con los organizadores para entrar otros coches");
         }
     }
 }
-
-//En esta clase existirá un método main con la siguiente funcionalidad
-//- Crea dos coches con los datos que tú quieras
-//- Crea una carrera con los datos que tú quieras
-//- Asocia los coches en la carrera
-//- Inicia la carrera y comprueba que los datos sean correcto
