bc454228b76a4b1a7d6c0e1bce0ed34e9838b6ef
David Duirs <duirsdevelopment@gmail.com>
2025-02-23 22:59:21 +0100

    Update Coche.java

--- programación/Carrera/Modelos/Coche.java
+++ programación/Carrera/Modelos/Coche.java
@@ -1,5 +1,8 @@
 package Modelos;
 
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Coche {
 
     private String marca, modelo, matricula;
@@ -7,7 +10,6 @@ public class Coche {
     private double zeroCien;
 
     public Coche() {
-        this(null, null, null, 0, 0, 0, 0, 0, 0);
     }
 
     public Coche(String marca, String modelo, String matricula, int cv, int cc, int velocidadMaxima, double zeroCien, int velocidad, int kms) {
@@ -86,48 +88,61 @@ public class Coche {
         return matricula;
     }
 
-    public void print(String color){ //mostrar datos
-        if(color == "yellow") {
+    public void print(int numero) {
+        System.out.print(" "+numero+".");
+        System.out.print("\033[0;33m    Marca = \033[0m" + marca);
+        System.out.print("\033[0;33m   Modelo = \033[0m" + modelo);
+        System.out.printf("\033[0;33m  Matrícula = \033[0m %s", matricula);
+        System.out.println();
+    }
+
+    public void print(String color) {
+        if (Objects.equals(color, "yellow")) {
             System.out.print("\033[0;33m Marca = \033[0m" + marca);
             System.out.print("\033[0;33m   Modelo = \033[0m" + modelo);
             System.out.print("\033[0;33m   CV = \033[0m" + cv);
             System.out.print("\033[0;33m    Velocidad máxima = \033[0m" + velocidadMaxima);
-            System.out.printf("%n\033[0;33m   Matrícula = \033[0m" + matricula);
+            System.out.printf("%n\033[0;33m  Matrícula = \033[0m %s", matricula);
             System.out.print("\033[0;33m                     CC = \033[0m" + cc);
             System.out.printf("\033[0;33m    Aceleración de 0-100 en %.1f s\033[0m", zeroCien);
-        }
-        else {
+        } else if (Objects.equals(color, "blue")) {
             System.out.print("\033[0;34m Marca = \033[0m" + marca);
             System.out.print("\033[0;34m   Modelo = \033[0m" + modelo);
             System.out.print("\033[0;34m   CV = \033[0m" + cv);
             System.out.print("\033[0;34m    Velocidad máxima = \033[0m" + velocidadMaxima);
-            System.out.printf("%n\033[0;34m Matrícula = \033[0m" + matricula);
+            System.out.printf("%n\033[0;34m  Matrícula = \033[0m %s", matricula);
             System.out.print("\033[0;34m                     CC = \033[0m" + cc);
             System.out.printf("\033[0;34m    Aceleración de 0-100 en %.1f s\033[0m", zeroCien);
+        } else {
+            System.out.print("\033[0;32m Marca = \033[0m" + marca);
+            System.out.print("\033[0;32m   Modelo = \033[0m" + modelo);
+            System.out.print("\033[0;32m   CV = \033[0m" + cv);
+            System.out.print("\033[0;32m    Velocidad máxima = \033[0m" + velocidadMaxima);
+            System.out.printf("%n\033[0;32m  Matrícula = \033[0m %s", matricula);
+            System.out.print("\033[0;32m                     CC = \033[0m" + cc);
+            System.out.printf("\033[0;32m    Aceleración de 0-100 en %.1f s\033[0m", zeroCien);
         }
     }
 
-    public void acelerar(int aceleracion){
-            int velocidadAcelerado = 0;
-            velocidadAcelerado = (int) (Math.random() * aceleracion);
-            if (aceleracion > 0) {
-                if((this.velocidad + velocidadAcelerado) <= this.velocidadMaxima) {
-                    if(velocidadAcelerado>10){
-                        this.velocidad += velocidadAcelerado;
-                    } else if (this.getCv() >= 100) {
-                        this.velocidad += velocidadAcelerado + 10;
+    public void acelerar(int acelerarRandom){
+            int velocidadAcelerado, velocidadQuerido = 0;
+            Scanner in = new Scanner(System.in);
+            System.out.print("  Introduce aceleración deseado:  ");
+            velocidadQuerido  = in.nextInt();
+            velocidadAcelerado = (velocidadQuerido * acelerarRandom)/100;
+            if (acelerarRandom > 0) {
+                if(velocidadAcelerado <= 10) {
+                    if (this.getCv() >= 100) {
+                        velocidadAcelerado = 11;
+                    } else {
+                        velocidadAcelerado = 10;
                     }
                 }
-                if(this.velocidad >= this.velocidadMaxima-20){
-                    System.out.println("    \033[0;33m   !!!!!!!!!! Velocidad Maxima casi alcanzado \033[0m");
-                }
-                System.out.println("        **** ** ** velocidadAcelerado = " + velocidadAcelerado);
-                this.kms += velocidad / 2;
-                System.out.printf("     car %s        kms recorridos %d %n", this.matricula, this.kms);
-            }
-            //para decelerar un velocidad con valor negativo
-            else if ((this.velocidad + velocidadAcelerado) > 100) {
                 this.velocidad += velocidadAcelerado;
+                this.setKms(this.getKms() + velocidadAcelerado/2);
+                System.out.printf("     Coche %s        Km recorridos = %d %n", this.matricula, this.kms);
             }
+
     }
+
 }
